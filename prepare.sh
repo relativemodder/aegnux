9e532ce31933dabbd181a07d1e05e50a4bc91b55
Andrew <kezin636@gmail.com>
2025-10-31 19:22:40 +0300

    no more blobs in the repo itself

--- prepare.sh
+++ prepare.sh
@@ -2,17 +2,20 @@
 
 
 # Download Kitty Binary
+echo Downloading Kitty Binary...
 curl -LO https://github.com/kovidgoyal/kitty/releases/download/v0.43.1/kitty-0.43.1-x86_64.txz
 mkdir -p ./bin/kitty && tar Jxf kitty-0.43.1-x86_64.txz --strip-components=0 -C ./bin/kitty
 rm kitty-0.43.1-x86_64.txz
 
 
 # Download winetricks
+echo Downloading Winetricks...
 curl -LO https://raw.githubusercontent.com/Winetricks/winetricks/refs/heads/master/src/winetricks
 mv winetricks ./bin/
 
 
 # Download cabextract
+echo Downloading Cabextract...
 curl -LO https://www.cabextract.org.uk/cabextract-1.11-1.x86_64.rpm
 bsdtar -xf cabextract-1.11-1.x86_64.rpm usr/bin/cabextract
 mv usr/bin/cabextract ./bin/cabextract
@@ -21,17 +24,25 @@ rm cabextract-1.11-1.x86_64.rpm
 
 
 # Download Wine
+echo Downloading Wine...
 curl -LO https://github.com/Kron4ek/Wine-Builds/releases/download/10.17/wine-10.17-amd64-wow64.tar.xz
 mkdir -p ./assets/wine && tar Jxf wine-10.17-amd64-wow64.tar.xz --strip-components=1 -C ./assets/wine
 rm wine-10.17-amd64-wow64.tar.xz
 
 
 # Download Visual C++ Redistributable Runtimes
+echo Downloading Visual C++ Redistributable Runtimes...
 curl -LO https://uk1-dl.techpowerup.com/files/qpMrHvhJCaISg-CxO9bnTg/1761969558/Visual-C-Runtimes-All-in-One-Jul-2025.zip
 mv Visual-C-Runtimes-All-in-One-Jul-2025.zip ./assets/vcr.zip
 
 
 # Download msxml3.zip bundle
-echo msxml3 downloading is not implemented yet.
+echo Downloading msxml3 dlls...
+curl -LO https://github.com/cutefishaep/AeNux/raw/refs/heads/main/asset/System32/msxml3.dll
+curl -LO https://github.com/cutefishaep/AeNux/raw/refs/heads/main/asset/System32/msxml3r.dll
+zip ./assets/msxml3.zip msxml3.dll msxml3r.dll
+rm msxml3.dll
+rm msxml3r.dll
+
 echo --------------------------------------------
 echo Done!
